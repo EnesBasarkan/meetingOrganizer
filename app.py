5cb2d12bd240193573c4763a4f5fcbe3bc47cc3a
enesbasarkan <enesbasarkan@gmail.com>
2021-05-19 14:00:11 +0300

    update,delete,add python function

--- app.py
+++ app.py
@@ -55,3 +55,41 @@ def insert():
  
         return redirect(url_for('Index'))
  
+#This route is for update data
+@app.route('/update', methods = ['GET', 'POST'])
+def update():
+ 
+    if request.method == 'POST':
+        my_data = Data.query.get(request.form.get('id'))
+ 
+        my_data.subject =request.form['subject']
+        my_data.date = request.form['date']
+        my_data.start_time =request.form['start_time']
+        my_data.end_time =request.form['end_time']
+        my_data.participants =request.form['participants']
+
+        db.session.commit()
+        flash("Meet Updated Successfully")
+ 
+        return redirect(url_for('Index'))
+ 
+ 
+ 
+ 
+#This route is for delete data
+@app.route('/delete/<id>/', methods = ['GET', 'POST'])
+def delete(id):
+    my_data = Data.query.get(id)
+    db.session.delete(my_data)
+    db.session.commit()
+    flash("Meet Deleted Successfully")
+ 
+    return redirect(url_for('Index'))
+ 
+ 
+ 
+ 
+ 
+ 
+if __name__ == "__main__":
+    app.run(debug=True)
